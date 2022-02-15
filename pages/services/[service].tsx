e91b68cdade57fa5c5edb9e34bb979f9ad53e5eb
ashuk2021 <ashuk@trakiot.in>
2022-02-15 20:42:42 +0530

    pages

---
+++ pages/services/[service].tsx
@@ -0,0 +1,114 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import Header from "../../components/Header";
+import styles from "./web-development.module.scss";
+import services, {
+  TechSectionInterface,
+  ServiceInterface,
+  Category,
+} from "../../components/constants/services";
+import Image from "next/image";
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths =
+    services &&
+    services.length > 0 &&
+    services.map((data, index) => ({
+      params: { service: data.path },
+    }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({
+  params,
+}: {
+  params: { service: string };
+}) => {
+  const service = services.find((element) => element.path === params.service);
+  return {
+    props: {
+      ...service,
+    },
+  };
+};
+
+const Service: NextPage<ServiceInterface> = ({
+  name,
+  smallDescription,
+  techDetails,
+  serviceCategories,
+}) => {
+  return (
+    <>
+      <Header />
+      <div className={styles.heroSection}>
+        <p className="primary-text">{name}</p>
+        <p className="normal-text">{smallDescription}</p>
+      </div>
+      {serviceCategories && (
+        <div className={`${styles.serviceCategoriesSection}`}>
+          <div className="container">
+            <p className="secondary-heading color-white">
+              {serviceCategories.title}
+            </p>
+            <p className="secondary-text color-white">
+              {serviceCategories.shortDescription}
+            </p>
+            <div className={styles.serviceCategoriesContainer}>
+              {serviceCategories &&
+                serviceCategories.categories.length > 0 &&
+                serviceCategories.categories.map(
+                  ({ title, icon, description }: Category) => {
+                    return (
+                      <div key={title} className={styles.serviceCategory}>
+                        <div className={styles.categoryImageContianer}>
+                          <Image src={icon} alt={title} />
+                        </div>
+                        <p className="secondary-text">{title}</p>
+                        <p className="normal-text">{description}</p>
+                      </div>
+                    );
+                  }
+                )}
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="container">
+        {techDetails &&
+          techDetails.length > 0 &&
+          techDetails.map((service, index) => {
+            return (
+              <TechSection key={`tech-section-detail-${index}`} {...service} />
+            );
+          })}
+      </div>
+    </>
+  );
+};
+
+const TechSection: NextPage<TechSectionInterface> = ({
+  image,
+  title,
+  description,
+}) => {
+  return (
+    <>
+      <div className={styles.techSectionContainer}>
+        <div className={styles.techSectionImageSection}>
+          <div className={styles.techSectionImageContainer}>
+            <Image src={image} alt="tech" className={styles.techSectionImage} />
+          </div>
+        </div>
+        <div className={styles.techSectionDetails}>
+          <p className="secondary-text">{title}</p>
+          <p className="normal-text">{description}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Service;
